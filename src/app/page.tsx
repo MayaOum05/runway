51ae77eed8c81d82cda8315c17a8427dbd7e5228
Maya Oum <maya.oum02@gmail.com>
2025-07-12 01:16:23 -0400

    bleh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Screen from "@/app/components/screen";
 
 export default function Page() {
   return (
-    <div>
-    
-      <Screen children={undefined} />
-    </div>
+    <Screen>
+      <p>This is a simple page using Next.js.</p>
+    </Screen>
   );
 }
